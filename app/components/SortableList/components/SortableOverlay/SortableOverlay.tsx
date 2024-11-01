47a2147cfc12b1fafa2aa8e7b1b7395ff959ffee
fakhergh <fakherghouili@gmail.com>
2024-11-01 21:52:09 +0400

    Changes

--- app/components/SortableList/components/SortableOverlay/SortableOverlay.tsx
+++ app/components/SortableList/components/SortableOverlay/SortableOverlay.tsx
@@ -1,23 +1,19 @@
-import type { DropAnimation } from '@dnd-kit/core';
-import { defaultDropAnimationSideEffects, DragOverlay } from '@dnd-kit/core';
-import type { PropsWithChildren } from 'react';
+import type { DropAnimation } from "@dnd-kit/core";
+import { defaultDropAnimationSideEffects, DragOverlay } from "@dnd-kit/core";
+import type { PropsWithChildren } from "react";
 
 const dropAnimationConfig: DropAnimation = {
-    sideEffects: defaultDropAnimationSideEffects({
-        styles: {
-            active: {
-                opacity: '0.4',
-            },
-        },
-    }),
+  sideEffects: defaultDropAnimationSideEffects({
+    styles: {
+      active: {
+        opacity: "0.4",
+      },
+    },
+  }),
 };
 
-interface Props {}
-
-export function SortableOverlay({ children }: PropsWithChildren<Props>) {
-    return (
-        <DragOverlay dropAnimation={dropAnimationConfig}>
-            {children}
-        </DragOverlay>
-    );
+export function SortableOverlay({ children }: PropsWithChildren) {
+  return (
+    <DragOverlay dropAnimation={dropAnimationConfig}>{children}</DragOverlay>
+  );
 }
