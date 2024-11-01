6fbf386a2bf060337af5a3759bec249bba4345ab
fakhergh <fakherghouili@gmail.com>
2024-11-01 22:58:41 +0400

    Changes

--- app/services/postService.ts
+++ app/services/postService.ts
@@ -41,15 +41,29 @@ export function useSortPosts() {
     mutationKey: ["update-post-position"],
     mutationFn: (data: UpdatePostPositionDto) =>
       PostsService.postsControllerSortsPosts(data),
-    onSuccess: (data) => {
-      return queryClient.setQueriesData<Array<Post>>(
-        {
-          predicate: (query) => {
-            return query.queryKey.includes("posts");
-          },
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({ queryKey: ["posts"] });
+
+      const prevPosts = queryClient.getQueryData<Array<Post>>(["posts"]);
+
+      const prevPostsById = prevPosts?.reduce(
+        (prev, curr: Post) => {
+          prev[curr._id] = curr;
+          return prev;
         },
-        () => data,
+        {} as Record<string, Post>,
       );
+
+      if (prevPostsById) {
+        queryClient.setQueryData(["posts"], () =>
+          variables.ids.map((id) => prevPostsById[id]),
+        );
+      }
+
+      return { prevPosts };
+    },
+    onError: (_, __, context: any) => {
+      queryClient.setQueriesData({ queryKey: ["posts"] }, context.prevPosts);
     },
   });
 }
